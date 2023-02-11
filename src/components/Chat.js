5ca2d650c9897b0870dd9003b215a7004122122b
ashwabu <abdulazizredi90@gmail.com>
2023-02-11 07:25:19 +0300

    text on display

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
-import { addDoc,collection,serverTimestamp } from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import { addDoc,collection,onSnapshot,query,serverTimestamp, where } from 'firebase/firestore'
 import { auth, db } from '../firebase'
-
+import '../styles/Chat.css'
 
 function Chat(props) {
     const {room} = props
     const [newMessage,setNewMessage] = useState('')
     const messagesRef = collection(db,'messages')
+    const [messages,setMessages] = useState([])
+
+    useEffect(()=>{
+        const queryRef = query(messagesRef,where('room','==',room))
+        const unsuscribe = onSnapshot(queryRef,(snapShot)=>{
+            let messages = [];
+            snapShot.forEach((doc)=>{
+                messages.push({...doc.data(),id: doc.id})
+            })
+            setMessages(messages)
+        })
+        return ()=>unsuscribe();
+    },[])
 
     const handleSubmit = async (e)=>{
         e.preventDefault()
@@ -21,13 +34,23 @@ function Chat(props) {
     }
   return (
     <div className='chat-app'>
+        <div className="header">
+            <h1>Welcome to: {room.toUpperCase()} </h1>
+        </div>
+        <div className='messages'>
+            {messages.map((message)=> 
+            <div className='message' key={message.id}>
+                <span className='user'>{message.user}  </span>
+                {message.text}
+            </div>
+        )} </div>
         <form onSubmit={handleSubmit} className='new-message-form'>
             <input 
             className='new-message-input'
             placeholder='Enter message'
             onChange={(e)=>setNewMessage(e.target.value)}
             value={newMessage}/>
-            <button type='submit'>send</button>
+            <button type='submit' className='send-button'>send</button>
         </form>
     </div>
   )
